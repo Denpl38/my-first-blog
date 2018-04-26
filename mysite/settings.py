514053ac1c179e3b60bf4a53eb426f79848c814f
Den_pl <Den_pl@mail.ru>
2018-04-26 11:50:45 +0800

    My Django app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ppixv9a8wiakptf#%gerdjh$3pl%r)@=wk_x!*3o*lmj@j!sy5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'denpl38.pythonanywhere.com']
 
 
 # Application definition
